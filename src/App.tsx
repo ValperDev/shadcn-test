9606548f42e8838416937e0f83011b9c86f5c82e
ValperDev <jdulcedev@gmail.com>
2024-08-05 22:07:20 -0500

    feat: add and test shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { Button } from './components/ui/button'
+import { BadgeDemo } from './components/demo/BadgeDemo'
+import { CarouselDemo } from './components/demo/CarouselDemo'
+import { CardWithForm } from './components/demo/CardWithForm'
+import { AccordionDemo } from './components/demo/AccordionDemo'
 
 function App() {
   return (
     <>
-      <div>
+      <div className='flex flex-col items-center justify-center gap-2'>
         <Button>Testing</Button>
+        <BadgeDemo></BadgeDemo>
+        <CarouselDemo></CarouselDemo>
+        <CardWithForm></CardWithForm>
+        <div className='w-[500px]'>
+          <AccordionDemo></AccordionDemo>
+        </div>
       </div>
     </>
   )
