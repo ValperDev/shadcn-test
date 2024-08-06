ad00d8bca786e663504fcba1738f547e8c8874db
ValperDev <jdulcedev@gmail.com>
2024-08-05 21:47:42 -0500

    initial files and shadcn install

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button>Testing</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
